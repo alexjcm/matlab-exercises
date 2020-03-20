e1703e63f024669c7835c1d0090f167b5ce6673c
AlexK <alexjhon@live.com>
2020-03-20 15:44:29 -0500

    Resolucion de funciones con Raphson

--- 23_metodoRaphsonV2.m
+++ 23_metodoRaphsonV2.m
@@ -3,9 +3,7 @@ clear, clc;
 %%
 
 disp('Metodo de Newton Raphson.')
-
 syms x;
-
 f=inline(68*x^5 + 5*x^4 - 22*x^3 + 13*x^2 - 102*x + 66);
 d=inline(340*x^4 + 20*x^3 - 66*x^2 + 26*x - 102);
 
@@ -13,6 +11,7 @@ x0 = 2;
 err = 0.001;
 err_abs = 100;
 iteracion = 0;
+
 while err_abs > err      
     %Aproximamos la raiz con la formula correpondiente
     x = x0 - (f(x0) / d(x0));    
@@ -24,6 +23,7 @@ while err_abs > err
     
     iteracion = iteracion + 1;
 end
+
 %Mostramos los resultados en pantalla (con 4 decimales)
 fprintf('\nRaiz= %10.4f en %d Iteraciones', x0, iteracion);
 
